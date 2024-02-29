7bfc30856beedd6cc5c6e9fa1395d584c1603ff9
Ramses Kamanda <ramses.kamanda@gmail.com>
2024-02-29 15:45:41 +0100

    Add full import paths to ldflags

--- main.go
+++ main.go
@@ -5,15 +5,7 @@ import (
 	"log"
 )
 
-var (
-	version = "dev"
-	commit  = "n/a"
-)
-
 func main() {
-	cmd.Version = version
-	cmd.Commit = commit
-
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
